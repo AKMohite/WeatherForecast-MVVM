44ccccdb9d4488ac4cb9bae2ec699d6d1822cbc4
Ashish Mohite <ashishmohite1996@gmail.com>
2026-07-27 11:24:18 +0530

    Network module created

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+  alias(libs.plugins.android.library)
+}
+
+android {
+  namespace = "app.mak.atmosense.core.network"
+  compileSdk = libs.versions.compileSDK.get().toInt()
+
+  defaultConfig {
+    minSdk = libs.versions.minSDK.get().toInt()
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+  }
+
+}
+
+dependencies {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,3 +24,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Atmosense"
 include(":app")
+include(":core:network")
