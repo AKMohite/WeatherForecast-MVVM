ddb306cf5d9e34bf73c50c64df2cd161e1e828c7
Ashish Mohite <ashishmohite1996@gmail.com>
2026-07-27 11:32:54 +0530

    Data module created

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+  alias(libs.plugins.android.library)
+}
+
+android {
+  namespace = "app.mak.atmosense.core.data"
+  compileSdk = libs.versions.compileSDK.get().toInt()
+
+  defaultConfig {
+    minSdk = libs.versions.minSDK.get().toInt()
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+  }
+
+}
+
+dependencies {}
