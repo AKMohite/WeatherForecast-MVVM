ade053d685f158a6c83d0584d6265aa8a00bcefc
Ashish Mohite <ashishmohite1996@gmail.com>
2026-07-31 13:39:52 +0530

    Add location module

---
+++ core/location/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+  alias(libs.plugins.android.library)
+  alias(libs.plugins.metro)
+}
+
+android {
+  namespace = "app.mak.atmosense.core.location"
+  compileSdk = libs.versions.compileSDK.get().toInt()
+
+  defaultConfig {
+    minSdk = libs.versions.minSDK.get().toInt()
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+  }
+
+}
+
+dependencies {
+  implementation(libs.play.services.location)
+  implementation(libs.kotlinx.coroutines.play.services)
+}
